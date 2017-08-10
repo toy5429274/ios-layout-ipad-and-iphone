93007c21ad43e807ec7be6bd66f364b8989b9754
chinnavon hamthanon <chinnavonhamthanon@chinnavons-MacBook-Pro.local>
2017-08-10 17:49:41 +0700

    first commit

---
+++ iphone and ipad size/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iphone and ipad size
+//
+//  Created by chinnavon hamthanon on 8/10/2560 BE.
+//  Copyright © 2560 chinnavon hamthanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
